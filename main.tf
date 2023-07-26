c5990a872f25b4aee146e1cecc5bf09a130dc85a
kevin wabo <kevin@MacBook-Pro-de-kevin.local>
2023-07-26 14:31:23 +0200

    starting the creating of the gke cluster

--- main.tf
+++ main.tf
@@ -1,10 +1,30 @@
 provider "google" {
     project= "deployment-dev-394008"
-    credentials = "${file("credentials.json")}"
+    credentials = "${file("credentials1.json")}"
     region= "us-central1"
     zone="us-central1-c"
 
 
+}
+resource "google_compute_instance" "default" {
+  name         = "test"
+  machine_type = "e2-medium"
+  zone         = "us-central1-c"
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+    boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      labels = {
+        my_label = "value"
+      }
+    }
+  }
 }
 
  
